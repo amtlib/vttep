1c31912929f660bb93da2f523bd16656ad7b1939
Grzegorz Pach <grzegorz.pach@schibsted.com>
2021-08-03 23:56:38 +0200

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="m-1 p-1">Arbeit!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import typescript from '@rollup/plugin-typescript';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import * as path from 'path';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: path.resolve(__dirname, './src'),
+      },
+    ],
+  },
+  // https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      fileName: 'main',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }),
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          declarationMap: false,
+          outDir: 'dist',
+          exclude: ['**/__tests__'],
+        }),
+      ],
+    },
+  },
+});
